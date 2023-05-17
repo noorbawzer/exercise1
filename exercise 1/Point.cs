29fb212cea59e976749d1811d4599e0d0f4d475d
noorbawzer <133597621+noorbawzer@users.noreply.github.com>
2023-05-18 01:20:16 +0300

    Add files via upload

---
+++ exercise 1/Point.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_1
+{
+    class Point
+    {
+        private double x;
+        private double y;
+        public Point(double x,double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void Move()
+        {
+            x = x - 5;
+            y = y + 5;
+            Console.WriteLine("x=" + x);
+            Console.WriteLine("y=" + y);
+        }
+        public void Quadrant()
+        {
+            if (x > 0 && y > 0)
+                Console.WriteLine("the coordinate point lise in the first quandrant.");
+           else if (x < 0 && y > 0)
+                Console.WriteLine("the coordinate point lise in the second quandrant.");
+          else  if (x < 0 && y < 0)
+                Console.WriteLine("the coordinate point lise in the third quandrant.");
+             else if (x > 0 && y < 0)
+                Console.WriteLine("the coordinate point lise in the fourth quandrant.");
+            else if (x == 0 && y == 0)
+                Console.WriteLine("the coordinate point lise in the origin quandrant");
+        }
+        public void Print()
+        {
+            Console.WriteLine("(" + x + "," + y + ")");
+        }
+    }
+}
---
+++ exercise 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the coordinates x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter the coordinates y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Point p = new Point(x,y);
+            p.Move();
+            p.Quadrant();
+            p.Print();
+        }
+    }
+}
